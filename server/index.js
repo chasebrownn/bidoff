70787dea171d1ae1b36132d61df09625ddb1a495
Henry O'Scannlain-Miller <henryom@me.com>
2021-10-21 21:47:43 -0700

    added a basic server with dotenv for configuring db connection locally.  basic get /auctions endpoint is written, does not sort yet

---
+++ server/index.js
@@ -0,0 +1,33 @@
+// MUST BE RUN FROM INSIDE SERVER
+// Run with "node index.js" or "nodemon index.js"
+
+require('dotenv').config()
+
+const express = require("express")
+const { Pool } = require("pg")
+const app = express()
+
+const cors = require("cors")
+const pool = require("./db")
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// ROUTES
+// get all auctions
+app.get("/auctions", async (req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM Auctions")
+        res.json(allTodos.rows)
+    } catch (err) {
+        res.send("error")
+        console.error(err.message)
+    }
+
+})
+
+app.listen(5000, () => {
+    console.log("Server started on port 5000");
+})
